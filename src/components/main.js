505383315c7df6d944da7379714840283c3eafd1
Tprad0330 <pradhan.code@gmail.com>
2024-09-06 16:30:23 -0400

    fix scroll and rows

--- src/components/main.js
+++ src/components/main.js
@@ -1,6 +1,6 @@
-import React from'react';
+import React, { useEffect } from 'react';
 import '../App.css';
-import Whale from './cards/whale.js'
+import Whale from './cards/whale.js';
 import Mushroom from './cards/mush.js';
 import Capybara from './cards/capybara.js';
 import Bracelet from './cards/bracelet.js';
@@ -9,26 +9,47 @@ import Blob from './cards/rock.js';
 import Glove from './cards/glove.js';
 
 function Main() {
+  useEffect(() => {
+    const containers = document.querySelectorAll('.card__container');
+    
+    const handleWheel = (event) => {
+      if (event.deltaY !== 0) {
+        event.currentTarget.scrollLeft += event.deltaY;
+        event.preventDefault();
+      }
+    };
+
+    containers.forEach(container => {
+      container.addEventListener('wheel', handleWheel);
+    });
+
+    return () => {
+      containers.forEach(container => {
+        container.removeEventListener('wheel', handleWheel);
+      });
+    };
+  }, []);
+
   return (
     <div className="App">
-        <div className="container">
-         <div className="card__container">
-            <Whale />
-            <Mushroom />
-            <Capybara />
-            <Bracelet />
-            <Shrimp />
-            <Blob />
-            <Glove />
-         </div>
-         <div className="card__container">
-            <Whale />
-            <Mushroom />
-            <Capybara />
-            <Bracelet />
-         </div>             
+      <div className="container">
+        <h3>Animals</h3>
+        <div className="card__container">
+          <Whale />
+          <Mushroom />
+          <Capybara />
+          <Bracelet />
+          <Shrimp />
+          <Blob />
+          <Glove />
+        </div>
+        <div className="card__container">
+          <Whale />
+          <Mushroom />
+          <Capybara />
+          <Bracelet />
+        </div>
       </div>
-      
     </div>
   );
 }
